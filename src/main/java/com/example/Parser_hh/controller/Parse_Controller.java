6049cc5099f593cf6ce128092c79be0a55a06ccb
IvanChet-4 <wokewylf-111@mail.ru>
2022-12-09 11:49:05 +0300

    Исправлено оформление.

--- src/main/java/com/example/Parser_hh/controller/Parse_Controller.java
+++ src/main/java/com/example/Parser_hh/controller/Parse_Controller.java
@@ -27,47 +27,37 @@ public class Parse_Controller {
     private Resume_Repo resume_repo;
 
     @GetMapping("all")
-    public ResponseEntity getParsers() {
+    public ResponseEntity getParsers() { // Вывод данных из БД на экран при нажатии "Вывести все значения из БД на экран"
         ArrayList<Resume_Entity> resume_entities = (ArrayList<Resume_Entity>) resume_repo.findAll();
         return ResponseEntity.ok(resume_entities);
     }
 
     @PostMapping("add")
-    public String startParse(@ModelAttribute ArrayList<Resume_Entity> resume_entity, @ModelAttribute Resume_Entity resume_entity_set) {
-
+    public String startParse(@ModelAttribute ArrayList<Resume_Entity> resume_entity, @ModelAttribute Resume_Entity resume_entity_set) { // Парсинг, запись в БД при нажатии "Запустить парсинг, добавить значения в БД"
         try {
+            for (int j = 0; j < Jtoe; j++) { // j - номер страницы, на каждой странице отображено 100 резюме
+                URL url = new URL(SITE_URL1 + j + SITE_URL2); // открываем соединение с сайтом (в константе SITE_URL зашит поисковый запрос, который можно изменить)
+                Document page = Jsoup.parse(url, 7000); // находим ссылку на резюме и достаем необходимую ее часть
+                    for (int i = 0; i < Itoe; i++) { // i - можно поставить 100
+                        Element listTagAForFindHref = page.select("a[class=serp-item__title]").get(i);//get(i)
+                        String attrTagA = listTagAForFindHref.attr("href");
+                        // System.out.println("listTagA = \n" + listTagA + "\n attrTagA = \n" + attrTagA);
+                        String[] splitAttrTagA = attrTagA.split("/");
+                        String[] splitResult0 = splitAttrTagA[2].split("\\?");
+                        // System.out.println(" split attrTagA = \n" + Arrays.toString(splitResult0));
 
-            // j - номер страницы, на каждой странице отображено 100 резюме
-            for (int j = 0; j < Jtoe; j++) {
+                        /* открываем резюме get(i); urlID - страница резюме*/
+                        URL urlID = new URL(SITE_URL_ID + splitResult0[0]);
+                        String siteWebPages = SITE_URL_ID + splitResult0[0];
+                        System.out.println("\n KeyWebPagesResume = " + siteWebPages);
+                        // URL urlID = new URL(URL_R);
+                        // URLConnection connectionID = urlID.openConnection();
+                        Document pageID = Jsoup.parse(urlID, 7000);
 
-                // открываем соединение с сайтом (в константе SITE_URL зашит поисковый запрос, который можно изменить)
-                URL url = new URL(SITE_URL1 + j + SITE_URL2);
-
-                // находим ссылку на резюме и достаем необходимую ее часть
-                Document page = Jsoup.parse(url, 7000);
-
-                // i - можно поставить 100
-                for (int i = 0; i < Itoe; i++) {
-
-                    Element listTagAForFindHref = page.select("a[class=serp-item__title]").get(i);//get(i)
-                    String attrTagA = listTagAForFindHref.attr("href");
-                    // System.out.println("listTagA = \n" + listTagA + "\n attrTagA = \n" + attrTagA);
-                    String[] splitAttrTagA = attrTagA.split("/");
-                    String[] splitResult0 = splitAttrTagA[2].split("\\?");
-                    // System.out.println(" split attrTagA = \n" + Arrays.toString(splitResult0));
-
-                    // открываем резюме get(i); urlID - страница резюме
-                    URL urlID = new URL(SITE_URL_ID + splitResult0[0]);
-                    String siteWebPages = SITE_URL_ID + splitResult0[0];
-                    System.out.println("\n KeyWebPagesResume = " + siteWebPages);
-                    // URL urlID = new URL(URL_R);
-                    // URLConnection connectionID = urlID.openConnection();
-                    Document pageID = Jsoup.parse(urlID, 7000);
-
-                    // was
-                    String[] wasTime = new String[0];
-                    String listTagDivForFindWas = pageID.select("div[class=resume-online-status]").text();
-                    if (!listTagDivForFindWas.isEmpty()) {
+                        // was
+                        String[] wasTime = new String[0];
+                        String listTagDivForFindWas = pageID.select("div[class=resume-online-status]").text();
+                        if (!listTagDivForFindWas.isEmpty()) {
                         wasTime = listTagDivForFindWas.split("&nbsp;");
                         // System.out.println("\n WasTime = " + Arrays.toString(wasTime));
                     }
@@ -75,36 +65,35 @@ public class Parse_Controller {
                     // age
                     int ageOnPageResume = 0;
                     Elements listTagSpanForFindAge = pageID.select("span[data-qa=resume-personal-age]");
-                    if (!listTagSpanForFindAge.isEmpty()) {
-                        String ageTagSpan = listTagSpanForFindAge.select("span").get(1).text();
-                        String[] splitAgeTagSpan = ageTagSpan.split(" ");
-                        ageOnPageResume = parseInt(splitAgeTagSpan[0]);
-                        // System.out.println("\n age = \n" + ageOnPageResume);
-                    }
+                        if (!listTagSpanForFindAge.isEmpty()) {
+                            String ageTagSpan = listTagSpanForFindAge.select("span").get(1).text();
+                            String[] splitAgeTagSpan = ageTagSpan.split(" ");
+                            ageOnPageResume = parseInt(splitAgeTagSpan[0]);
+                            // System.out.println("\n age = \n" + ageOnPageResume);
+                         }
 
                     // position
                     String listTagSpanForFindPosition = pageID.select("span[data-qa=resume-block-title-position]").text();
-                    if (!listTagSpanForFindPosition.isEmpty()) {
-                        // System.out.println("\n Position = \n" + listTagSpanForFindPosition);
-                    }
+                        if (!listTagSpanForFindPosition.isEmpty()) {
+                            // System.out.println("\n Position = \n" + listTagSpanForFindPosition);
+                        }
 
-/////////////////////////////////////////////
-                    // name
-//        Elements listTagSpanForFindName = pageID.select("h2[data-qa=resume-personal-name]");
-//        String nameTagSpan = listTagSpanForFindName.select("span").text();
-//        String[] splitNameTagSpan = nameTagSpan.split(" ");
-//        String splitNameTagSpan0 = splitNameTagSpan[0];
-//        System.out.println("\n splitNameTagSpan0 = \n" + splitNameTagSpan0);
-//        System.out.println(listTagSpanForFindName);
-                    // surname
-//        String splitNameTagSpan1 = splitNameTagSpan[1];
-//        System.out.println("\n splitNameTagSpan1 = \n" + splitNameTagSpan1);
-                    // email
-//        Elements listTagDivForFindEmail = pageID.select("div[data-qa=resume-contact-email]");
-//        String emailTagSpan = listTagDivForFindEmail.select("span").text();
-                    // phone
-//        String listTagSpanForFindPhone = pageID.select("span[data-qa=resume-contact-preferred]").text();
-/////////////////////////////////////////////
+/* name
+* Elements listTagSpanForFindName = pageID.select("h2[data-qa=resume-personal-name]");
+* String nameTagSpan = listTagSpanForFindName.select("span").text();
+* String[] splitNameTagSpan = nameTagSpan.split(" ");
+* String splitNameTagSpan0 = splitNameTagSpan[0];
+* System.out.println("\n splitNameTagSpan0 = \n" + splitNameTagSpan0);
+* System.out.println(listTagSpanForFindName);
+* surname
+* String splitNameTagSpan1 = splitNameTagSpan[1];
+* System.out.println("\n splitNameTagSpan1 = \n" + splitNameTagSpan1);
+* email
+* Elements listTagDivForFindEmail = pageID.select("div[data-qa=resume-contact-email]");
+* String emailTagSpan = listTagDivForFindEmail.select("span").text();
+* phone
+* String listTagSpanForFindPhone = pageID.select("span[data-qa=resume-contact-preferred]").text();
+*/
 
                     // dateNow
                     LocalDateTime dateNow = LocalDateTime.now();
@@ -117,7 +106,6 @@ public class Parse_Controller {
                             "\n dateNow = " + dateNow +
                             "\n Arrays.toString(wasTime) = " + Arrays.toString(wasTime));
 
-
                     // Results save
                     resume_entity_set = new Resume_Entity();
                     resume_entity_set.setAge(ageOnPageResume);
@@ -126,26 +114,24 @@ public class Parse_Controller {
                     resume_entity_set.setLocal_time(dateNow);
                     resume_entity_set.setWas_time(Arrays.toString(wasTime));
 
-                    // Без ключа, блок с этими данными закрыт
-                    // resume_entity_set.setName(splitNameTagSpan[0]);
-                    // resume_entity_set.setSurname(splitNameTagSpan[1]);
-                    // resume_entity_set.setEmail(emailTagSpan);
-                    // resume_entity_set.setPhone(listTagSpanForFindPhone);
+/* Без ключа, блок с этими данными закрыт
+* resume_entity_set.setName(splitNameTagSpan[0]);
+* resume_entity_set.setSurname(splitNameTagSpan[1]);
+* resume_entity_set.setEmail(emailTagSpan);
+* resume_entity_set.setPhone(listTagSpanForFindPhone);
+*/
 
-                    // Записываем результат
-                    resume_entity.add(resume_entity_set);
-
+                    resume_entity.add(resume_entity_set); // Записываем результат
                 }
-            }// Закрываются циклы for
+            } // Закрываются циклы for
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-        // Сохраняем результат
-        startSave(resume_entity);
+        startSave(resume_entity); // Сохраняем результат
         return "БД обновлена";
     }
 
-    private void startSave(ArrayList<Resume_Entity> resume_entity) {
+    private void startSave(ArrayList<Resume_Entity> resume_entity) { // Сохранение результатов парсинга в БД
         for (int i = 0; i < resume_entity.size(); i++) {
             resume_repo.save(resume_entity.get(i));
             // System.out.println( "\n get i \n" + resume_entity.get(i).toString());
--- src/main/java/com/example/Parser_hh/entity/Resume_Entity.java
+++ src/main/java/com/example/Parser_hh/entity/Resume_Entity.java
@@ -6,7 +6,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
-
 @Entity
 public class Resume_Entity {
     @Id
@@ -22,85 +21,64 @@ public class Resume_Entity {
     LocalDateTime local_time;
     String was_time;
 
-    public Resume_Entity() {
-    }
-
+    public Resume_Entity() { }
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getName() {
         return name;
     }
-
     public void setName(String name) {
         this.name = name;
     }
-
     public String getSurname() {
         return surname;
     }
-
     public void setSurname(String surname) {
         this.surname = surname;
     }
-
     public int getAge() {
         return age;
     }
-
     public void setAge(int age) {
         this.age = age;
     }
-
     public String getPosition() {
         return position;
     }
-
     public void setPosition(String position) {
         this.position = position;
     }
-
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
-
     public String getUrl_site() {
         return url_site;
     }
-
     public void setUrl_site(String url_site) {
         this.url_site = url_site;
     }
-
     public LocalDateTime getLocal_time() {
         return local_time;
     }
-
     public void setLocal_time(LocalDateTime local_time) {
         this.local_time = local_time;
     }
-
     public String getWas_time() {
         return was_time;
     }
-
     public void setWas_time(String was_time) {
         this.was_time = was_time;
     }
